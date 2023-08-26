2e52fcbc2b7082f2b010f52e4b8f42b11b4f25bc
dmytrove <dmytro.verzhbytskyi@sectigo.com>
2023-08-27 00:43:22 +0300

    refactored containsDuplicates with lower complexity using HashSet

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
@@ -7,8 +9,10 @@ public class Main {
         System.out.println("getIndexesByNumber " + Arrays.toString(getIndexesByNumber(20, 3, 8, 15, 17)));
         System.out.println("getIndexesByNumber " + Arrays.toString(getIndexesByNumber(6, 3, 2, 3, 10)));
 
-        System.out.println("containsDuplicates " + containsDuplicates(4, 5, 6, 6, 8));
-        System.out.println("containsDuplicates " + containsDuplicates(4, 5, 6, 7, 8));
+        System.out.println("containsDuplicatesV1 " + containsDuplicatesV1(4, 5, 6, 6, 8));
+        System.out.println("containsDuplicatesV1 " + containsDuplicatesV1(4, 5, 6, 7, 8));
+        System.out.println("containsDuplicatesV2 " + containsDuplicatesV2(4, 5, 6, 6, 8));
+        System.out.println("containsDuplicatesV2 " + containsDuplicatesV2(4, 5, 6, 7, 8));
     }
 
     static int[] getIndexesByNumber(int number, int... array) {
@@ -45,11 +49,11 @@ public class Main {
             if (array[j - 1] + array[array.length / 2] == number) {
                 return new int[]{array.length / 2, j - 1};
             }
-            if (array[j] + array[array.length / 2+1] == number) {
-                return new int[]{array.length / 2+1, j};
+            if (array[j] + array[array.length / 2 + 1] == number) {
+                return new int[]{array.length / 2 + 1, j};
             }
-            if (array[j - 1] + array[array.length / 2+1] == number) {
-                return new int[]{array.length / 2+1, j - 1};
+            if (array[j - 1] + array[array.length / 2 + 1] == number) {
+                return new int[]{array.length / 2 + 1, j - 1};
             }
             if (switcher) {
                 switcher = false;
@@ -62,13 +66,38 @@ public class Main {
         return new int[0];
     }
 
-    static boolean containsDuplicates(int... array) {
-        for (int i = 0; i < array.length - 1; i++) {
-            for (int j = i + 1; j < array.length; j++) {
-                if (array[i] == array[j]) {
-                    return true;
-                }
+    // both of these solutions passes all the tests from leetcode
+    static boolean containsDuplicatesV1(int... array) {
+        if (array.length <= 1) {
+            return false;
+        }
+        if (array.length == 2) {
+            return array[0] == array[1];
+        }
+        int[] sorted = Arrays.stream(array).sorted().toArray();
+        for (int i = 0; i < sorted.length - 1; i++) {
+            if (sorted[i] == sorted[i + 1]) {
+                return true;
             }
+        }
+        return false;
+    }
+
+    // this solution is faster
+    static boolean containsDuplicatesV2(int... array) {
+        if (array.length <= 1) {
+            return false;
+        }
+        if (array.length == 2) {
+            return array[0] == array[1];
+        }
+        Set<Integer> noDuplicates = new HashSet<>();
+        noDuplicates.add(array[0]);
+        for (int i = 1; i < array.length; i++) {
+            if (noDuplicates.contains(array[i])) {
+                return true;
+            }
+            noDuplicates.add(array[i]);
         }
         return false;
     }
